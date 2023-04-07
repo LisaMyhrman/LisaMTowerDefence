8b5edadc9c76b0bd3882359a0c397d8882f04a1a
LisaMyhrman <lisa@myhrman.me>
2023-04-07 16:06:35 +0200

    game1 into gamemanager

--- Game1.cs
+++ Game1.cs
@@ -18,39 +18,7 @@ namespace LisaMTowerDefence
         private int windowWidth = 1200;
         private int windowHeight = 900;
 
-        //spline
-        private SimplePath path;
-        
-
-        //textures
-        private Texture2D catTex;
-        private float enemyStartPos;
-        private Texture2D tinyCatTex;
-
-        //rendertarget
-        private Texture2D transTex;
-        private RenderTarget2D renderTest;
-
-        //object following mouse(currently selected object)
-        private Vector2 mousePos;
-        private MouseState mouseState;
-        private KeyboardState keyState;
-
-        //enemies
-        private Enemy enemy;
-        private List<Enemy> enemies = new List<Enemy>();
-
-        //towers
-        private Tower test;
-        private Tower chosenTower = null;
-
-        //ska listan innehålla almänna towers, beroende på olika typer osvosv?
-        List<Tower> placedObjects = new List<Tower>();
-
-        //shootingtests
-        private List<Bullet> bullets = new List<Bullet>();
-
-        
+        GameManager gameManager;
 
         public Game1()
         {
@@ -61,10 +29,8 @@ namespace LisaMTowerDefence
 
         protected override void Initialize()
         {
-         
-            path = new SimplePath(graphics.GraphicsDevice);
-            path.Clean();
-    
+            gameManager = new GameManager();
+
             base.Initialize();
         }
 
@@ -73,61 +39,9 @@ namespace LisaMTowerDefence
             graphics.PreferredBackBufferHeight = windowHeight;
             graphics.PreferredBackBufferWidth = windowWidth;
             graphics.ApplyChanges();
-
-            Assets.LoadTextures(Content);
-
-            //behövs inte senare
-            tinyCatTex = Assets.TinyCatTex;
-            catTex = Assets.catTex;
-
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-
-            //catTex = Content.Load<Texture2D>("fatcat");
-            //transTex = Content.Load<Texture2D>("transparentSquareBackground");
-            //tinyCatTex = Content.Load<Texture2D>("cat");
-
-           
-
-
-            renderTest = new RenderTarget2D(GraphicsDevice, Window.ClientBounds.Width, Window.ClientBounds.Height);
-
-            
-            mousePos = new Vector2(mouseState.X, mouseState.Y);
-
-//SKAPA ENDAST DÅ TORN VALTS
-            //test = new Tower(catTex, new Vector2(0,0), new Rectangle(0, 0, catTex.Width, catTex.Height));
-
-            
-            
-            DrawOnRenderTarget();
-
-
-//MAKE PRETTIER PATH
-//MAKE METHOD FOR PATH
-
-            path.AddPoint(Vector2.Zero);
-            path.SetPos(0,Vector2.Zero);
-            path.AddPoint(new Vector2(100, 100));
-            path.AddPoint(new Vector2(200, 100));
-            path.AddPoint(new Vector2(200, 200));
-            path.AddPoint(new Vector2(windowWidth - 100, windowHeight));
-            path.AddPoint(new Vector2(windowWidth, windowHeight));
-
-
-
-            //FIX LISTED ENEMIES
-
-            enemyStartPos = path.beginT;
-            enemies.Add(new Enemy(tinyCatTex, new Vector2(enemyStartPos, enemyStartPos), new Rectangle(0, 0, tinyCatTex.Height, tinyCatTex.Width), 2.0f, 2, 3));
-            enemies.Add(new Enemy(tinyCatTex, new Vector2(enemyStartPos, enemyStartPos), new Rectangle(0, 0, tinyCatTex.Height, tinyCatTex.Width), 1.0f, 2, 1));
-            //enemy = new Enemy(tinyCatTex, new Vector2(catPos, catPos), new Rectangle(0, 0, tinyCatTex.Height, tinyCatTex.Width));
-
-            foreach (Enemy e in enemies)
-            {
-                e.positionOnPath = path.beginT;
-            }
-
+            gameManager.LoadGame(GraphicsDevice, Content, spriteBatch);
         }
         
         protected override void Update(GameTime gameTime)
@@ -135,235 +49,15 @@ namespace LisaMTowerDefence
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            mouseState = Mouse.GetState();
-            keyState = Keyboard.GetState();
-            mousePos.X = mouseState.X;
-            mousePos.Y = mouseState.Y;
-
-            System.Diagnostics.Debug.WriteLine(EconomyTracker.GetCoins());
-
-
-
-
-            //if 1,2,3,4(corresponding to towertypes) is pressed, spawn new tower ( MAKE METHOD)
-
-            if(chosenTower == null)
-            {
-//FIX COST OF TOWERS
-                if (keyState.IsKeyDown(Keys.E))// && chosenTower.Cost < EconomyTracker.GetCoins())
-                {
-                    //standardtower
-                    //chosenTower = new Tower(catTex, new Vector2(0, 0), new Rectangle(0, 0, catTex.Width, catTex.Height));
-                    chosenTower = new Tower(catTex, mousePos, new Rectangle((int)mousePos.X, (int)mousePos.Y, catTex.Width, catTex.Height),1, 4000, 300);
-                }
-                else if(keyState.IsKeyDown(Keys.F))
-                {
-                    chosenTower = new Tower(catTex, mousePos, new Rectangle((int)mousePos.X, (int)mousePos.Y, catTex.Width, catTex.Height),2, 1000, 500);
-                }
-
-//POSSIBILITY FOR DESIGN YOUR OWN TOWER
-                
-            }
-            else
-            {
-                chosenTower.pos = mousePos;
-                chosenTower.hitbox.X = (int)mousePos.X;
-                chosenTower.hitbox.Y = (int)mousePos.Y;
-            }
-            
-
-            //METHOD?
-            if (mouseState.LeftButton == ButtonState.Pressed && chosenTower != null)
-            {
-                if (CanPlace(chosenTower) == true)
-                {
-//MIGHT BE A PROBLEM WITH LIST NULLING?
-                    //placedObjects.Add(new Tower(catTex, mousePos, new Rectangle((int)mousePos.X, (int)mousePos.Y, catTex.Width, catTex.Height)));
-                    placedObjects.Add(chosenTower);
-                    DrawOnRenderTarget();
-                    chosenTower = null;
-                  
-                }
-            }
-                
-
-
-            for (int i = 0; i < enemies.Count; i++)
-            {
-                enemies[i].pos = path.GetPos(enemies[i].positionOnPath);
-                enemies[i].Update();
-                //System.Diagnostics.Debug.WriteLine(enemies[i].Health);
-                if (enemies[i].Health <= 0)
-                {
-                    EconomyTracker.AlterCoins(enemies[i].Value);
-                    enemies.RemoveAt(i);
-                }
-            }
-
-            foreach (Tower t in placedObjects)
-            {
-                t.Update(gameTime);
-                if(CheckDistance(t))
-                {
-                    //System.Diagnostics.Debug.WriteLine("in distance");
-                    if(t.isShooting)
-                    {
-                        bullets.Add(t.typeOfBullet);
-                    }
-                }
-                
-            }
-
-            for (int i = 0; i < bullets.Count; i++)
-            {
-                bullets[i].Update();
-                BulletCollision(bullets[i]);
-            }
+            gameManager.Update(gameTime);
 
             base.Update(gameTime);
         }
 
-      
-
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
-
-            spriteBatch.Begin();
-            path.Draw(spriteBatch);
-            //path.DrawPoints(spriteBatch);
-
-            spriteBatch.Draw(renderTest, Vector2.Zero, Color.White);
-
-            if(chosenTower != null)
-            {chosenTower.Draw(spriteBatch);}
-            
-
-
-            //DRAW BULLETS NOT WORKING
-
-            for (int i = 0; i < bullets.Count; i++)
-            {
-                bullets[i].Draw(spriteBatch);
-            }
-
-            //foreach(Bullet b in bullets)
-            //{
-            //    b.Draw(spriteBatch);
-            //}
-
-            foreach (Enemy e in enemies)
-            {
-                if (enemyStartPos < path.endT)
-                {
-                    //BEHÖVER DENNA VARA SÅ GODDAMN LONG?
-                    //ENDA SOM ANVÄNDS ÄR ORIGIN
-                    e.Draw(spriteBatch);
-                //enemy.Draw(spriteBatch);
-                //spriteBatch.Draw(tinyCatTex, path.GetPos(catPos), new Rectangle(0, 0, tinyCatTex.Width, tinyCatTex.Height), Color.White, 0f, new Vector2(tinyCatTex.Width / 2, tinyCatTex.Height / 2), 1f, SpriteEffects.None, 0f);
-                }
-            }
-
-            spriteBatch.End();
-
+            gameManager.Draw(spriteBatch, GraphicsDevice);
             base.Draw(gameTime);
-        }
-
-        private void DrawOnRenderTarget()
-        {
-            GraphicsDevice.SetRenderTarget(renderTest);
-
-            GraphicsDevice.Clear(Color.Transparent);
-            spriteBatch.Begin();
-
-            foreach (var obj in placedObjects)
-            {
-                obj.Draw(spriteBatch);
-            }
-
-            //GraphicsDevice.Clear(Color.Transparent);
-
-            //spriteBatch.Draw(transTex, Vector2.Zero, Color.White);
-            //spriteBatch.Draw(catTex, new Vector2(100,100), Color.White);
-            spriteBatch.End();
-
-            GraphicsDevice.SetRenderTarget(null);
-        }
-
-        public bool CanPlace(GameObject g)
-        {
-//BUG: CLICKA UTANFÖR WINDOW = CRASH, SOVLED, TOO LONG STATEMENT?
-            if (mousePos.X > 0 && mousePos.X + g.tex.Width < windowWidth && mousePos.Y > 0 && mousePos.Y + g.tex.Height < windowHeight)
-            {
-                Color[] pixels = new Color[g.tex.Width * g.tex.Height];
-                Color[] pixels2 = new Color[g.tex.Height * g.tex.Width];
-                g.tex.GetData<Color>(pixels2);
-                renderTest.GetData(0, g.hitbox, pixels, 0, pixels.Length);
-                //System.Diagnostics.Debug.WriteLine("pixels" + pixels[0].A.ToString());
-                //System.Diagnostics.Debug.WriteLine("pixels2" + pixels2[0].A.ToString());
-                for (int i = 0; i < pixels.Length; i++)
-                {
-                    if (pixels[i].A > 0.0f && pixels2[i].A > 0.0f)
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        //private void ShootBullet()
-        //{
-
-        //}
-
-        private bool CheckDistance(Tower t)
-        {
-                //Check through list of enemies
-                //else-statement? none
-
-            foreach(Enemy e in enemies)
-            {
-//PICK DISTANCE HERE
-                if(GetDistance(t.GetTowerPos, e.pos) < t.Reach)
-                {
-                    t.ClosestEnemyPos = e.pos;
-                    //System.Diagnostics.Debug.WriteLine(enemy.pos);
-                    return true;
-
-                }
-                else
-                {
-                    continue;
-                }
-            }
-
-            return false;
-
-        }
-
-        private float GetDistance(Vector2 one, Vector2 two)
-        {
-            float X = one.X - two.X;
-            float Y = one.Y - two.Y;
-            return (float)Math.Sqrt((X*X) + (Y*Y));
-        }
-
-        private void BulletCollision(Bullet b)
-        {
-            foreach(Enemy e in enemies)
-            {
-                if(b.IsColliding(e.hitbox))
-                {
-                    System.Diagnostics.Debug.WriteLine("HIT");
-                    e.HealthMaster(b.Damage);
-                    bullets.Remove(b);
-                }
-            }
         }
     }
 }
--- Tower.cs
+++ Tower.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
 
 //MAKE UNDERCLASSES DEPENDING ON WHAT KIND OF TOWER
 
@@ -26,20 +27,37 @@ namespace LisaMTowerDefence
         private int cost;
         private int reloadShotMiliSec;
         private int reach;
+        private int towerType;
 
 
-        public Tower(Texture2D texture, Vector2 position, Rectangle hitbox, int cost, int reloadShotMiliSec, int reach) : base(texture, position, hitbox)
+        public Tower(Texture2D texture, Vector2 position, Rectangle hitbox, int towerType) : base(texture, position, hitbox)
         {
             timer = 0;
             shooting = false;
             midPos = new Vector2(position.X + texture.Width / 2, position.Y + texture.Height / 2);
             closestEnemyPos = new Vector2(0, 0);
             //SPECIFIK FOR EACH TOWERTYPE
-            this.cost = cost;
-            this.reloadShotMiliSec = reloadShotMiliSec;
-            this.reach = reach;
+            //this.cost = cost;
+            //this.reloadShotMiliSec = reloadShotMiliSec;
+            //this.reach = reach;
+            //int cost, int reloadShotMiliSec, int reach
+            this.towerType = towerType;
 
-            
+            //SWITCHCASE
+            if(towerType == 1)
+            {
+                cost = 1;
+                reach = 300;
+                reloadShotMiliSec = 2000;
+            }
+            else if(towerType == 2)
+            {
+                cost = 2;
+                reach = 600;
+                reloadShotMiliSec = 500;
+            }
+
+
 
 
             //cost = 1;
@@ -58,7 +76,10 @@ namespace LisaMTowerDefence
 
         private void Shooting()
         {
-            if(timer >= reloadShotMiliSec)
+//INTE SNYGGAST ATT HA MIDPOS HÄR
+            midPos.X = (pos.X + tex.Width / 2);
+            midPos.Y = (pos.Y + tex.Height / 2);
+            if (timer >= reloadShotMiliSec)
             {
                 System.Diagnostics.Debug.WriteLine("shot");
                 timer = 0;
@@ -88,16 +109,17 @@ namespace LisaMTowerDefence
             set { shooting = value; }
         }
 
+
+//MAKE LIST OF TOWERS + THEIR TYPE OF BULLET
         public Bullet typeOfBullet
         {
             get { return new Bullet(Assets.TinyCatTex, midPos, new Rectangle((int)midPos.X, (int)midPos.Y ,Assets.TinyCatTex.Width, Assets.TinyCatTex.Height), GetDirection(), 2, 1); }
         }
 
-
-//MIDDLEPOSITION FUCKING UP
         public Vector2 GetTowerPos
         {
             get { return midPos; }
+            set { pos = value; }
         }
 
         public Vector2 ClosestEnemyPos
